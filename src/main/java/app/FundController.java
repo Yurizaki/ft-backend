2fe6a18c95b1b69ae2661543d59a255934d62cb7
alex <alxdev94@gmail.com>
2021-03-06 00:39:50 +0000

    cors localhost support for testing

--- src/main/java/app/FundController.java
+++ src/main/java/app/FundController.java
@@ -3,11 +3,12 @@ package app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class FundController {
     
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/fund")
 	public String fund(
         @RequestParam(value = "ticker", defaultValue = "IITU") String ticker) {
